1cf94308ed29cae3b5931c898f2040b0b4e98445
Joshua B <dogpetkid@gmail.com>
2020-01-25 22:43:59 -0700

    4 kinds of constants
    Differentiate between 4 kinds of constatnts; Constants: field or actual
    constant values; IO: input output constants; Map: ids of motors,
    sensors, etc; Vars: things about the behaviour of the robot

---
+++ src/main/java/frc/robot/Vars.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+/**
+ * All variables tied to the physical behavior of the robot. Tuning, reverse booleans, etc.
+ */
+public class Vars {
+
+	public static final double MAX_VELOCITY = 114; // inches/sec
+	public static final double MAX_ACCELERATION = 220; // inches/sec^2
+
+  public static final boolean LEFT_DRIVE_INVERTED = false;
+  public static final boolean RIGHT_DRIVE_INVERTED = false;
+
+}
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -19,13 +19,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.IO;
+import frc.robot.RobotMap;
+import frc.robot.Vars;
 
 public class DrivetrainSubsystem extends SubsystemBase {
-  
-  private static final int ID_FRONTLEFT = 14;
-  private static final int ID_BACKLEFT = 15;
-  private static final int ID_FRONTRIGHT = 1;
-  private static final int ID_BACKRIGHT = 0;
 
   /**
    * Resolution of the encoders.
@@ -51,20 +48,21 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   public DrivetrainSubsystem() {
 
-    FrontLeft = new WPI_TalonFX(ID_FRONTLEFT);
-    BackLeft = new WPI_TalonFX(ID_BACKLEFT);
-    FrontRight = new WPI_TalonFX(ID_FRONTRIGHT);
-    BackRight = new WPI_TalonFX(ID_BACKRIGHT);
+    FrontLeft = new WPI_TalonFX(RobotMap.ID_FRONTLEFT);
+    BackLeft = new WPI_TalonFX(RobotMap.ID_BACKLEFT);
+    FrontRight = new WPI_TalonFX(RobotMap.ID_FRONTRIGHT);
+    BackRight = new WPI_TalonFX(RobotMap.ID_BACKRIGHT);
 
     FrontLeft.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, Constants.PRIMARY_PID, Constants.MS_TIMEOUT);
     FrontRight.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, Constants.PRIMARY_PID, Constants.MS_TIMEOUT);
-    FrontLeft.setSensorPhase(false);
-    FrontRight.setSensorPhase(false);
+    // Setting the sensor phase is not important as the Differential drive
+    // makes the values that come from the right side flipped regardless;
+    // a manual flip is located on the periodic
 
     LeftGroup = new SpeedControllerGroup(FrontLeft, BackLeft);
     RightGroup = new SpeedControllerGroup(FrontRight, BackRight);
-    LeftGroup.setInverted(false);
-    RightGroup.setInverted(false);
+    LeftGroup.setInverted(Vars.LEFT_DRIVE_INVERTED);
+    RightGroup.setInverted(Vars.RIGHT_DRIVE_INVERTED);
 
     drive = new DifferentialDrive(LeftGroup, RightGroup);
 
@@ -212,7 +210,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    
     if (IO.verbose) putDashboard();
     PERIODICio.angle = navx.getAngle();
     PERIODICio.leftEnc = FrontLeft.getSelectedSensorPosition();
@@ -221,6 +219,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
     // so the motor is still backwards even though we give the differential drive a positive command
     PERIODICio.leftEncVelocity = FrontLeft.getSelectedSensorVelocity();
     PERIODICio.rightEncVelocity = FrontRight.getSelectedSensorVelocity() * -1;
+    // see above
 
   }
 
