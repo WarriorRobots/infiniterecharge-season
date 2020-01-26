6c42ff08c4666da458b133e2466e7736b5b78a9d
Joshua B <dogpetkid@gmail.com>
2020-01-25 23:40:37 -0700

    Create Trajectory base
    Streamline the process of making trajectories by making a class to
    handle all the dirty work of a trajectory

---
+++ src/main/java/frc/robot/commands/drive/trajectories/TBase.java
@@ -0,0 +1,111 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.drive.trajectories;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.util.Units;
+import frc.robot.Vars;
+
+/**
+ * Base trajectory class for 2478.
+ * Streamlines the process of making a {@link Trajectory} using the WPI tools.
+ * <p>
+ * Requires at least a {@link #build()} function to set the {@link #start}, {@link #end},
+ * and {@link #Waypoints}.
+ * 
+ * @author Joshua B.
+ */
+public abstract class TBase {
+
+  /** Max speed of trajectory */
+  static final double MAX_SPEED = maxSpeed();
+  /** Max acceleration of trajectory (in m/s) */
+  static final double MAX_ACCELERATION = maxAcceleration();
+  /** Whether the robot drives backwards along the path */
+  static final boolean REVERSED = isReversed();
+
+  /** Pose of the robot at the start of the trajectory. */
+  Pose2d start;
+  /** Pose of the robot at the end of the trajectory. */
+  Pose2d end;
+  /** All interior waypoints the robot must pass through on the trajectory. */
+  ArrayList<Translation2d> Waypoints = new ArrayList<Translation2d>();
+  /** Configuration of the trajectory, involves the speed, acceleration, and direction of the trajectory. */
+  TrajectoryConfig config;
+
+  /** Final trajectory produced by the class. */
+  Trajectory trajectory;
+
+  /**
+   * Returns the max speed the robot can travel on the trajectory. If not
+   * overrided, returns the max speed of the robot. NOTE: This is not the speed it
+   * follows the trajectory but the actual max capable speed.
+   * 
+   * @return max speed of the robot in m/s.
+   */
+  static double maxSpeed() {
+    return Units.inchesToMeters(Vars.MAX_VELOCITY);
+  }
+
+  /**
+   * Returns the max acceleration the robot can travel on the trajectory. If not
+   * overrided, returns the max acceleration of the robot. NOTE: This is not the
+   * acceration it follows the trajectory but the actual max capable acceration.
+   * 
+   * @return max acceleration of the robot in m/s^2.
+   */
+  static double maxAcceleration() {
+    return Units.inchesToMeters(Vars.MAX_ACCELERATION);
+  }
+
+  /**
+   * Returns if the robot travels backwards on the trajectory. Traveling backwards
+   * means the robot still goes through the points in order, but in the reversed
+   * order. Travelling reversed also means the heading of the poses are still the
+   * forwards direction of the robot. If not overrided, returns false.
+   * 
+   * @return whether the robot travels backwards on the trajectory.
+   */
+  static boolean isReversed() {
+    return false;
+  }
+
+  /**
+   * Sets the start pose, end pose, and any waypoints inbetween.
+   * @see #start
+   * @see #end
+   * @see #Waypoints
+   */
+  abstract void build();
+
+  /**
+   * Constructs path from given start, end, and waypoints from {@link build()}.
+   */
+  public TBase() {
+    build();
+
+    config = new TrajectoryConfig(MAX_SPEED, MAX_ACCELERATION);
+    config.setReversed(REVERSED);
+
+    trajectory = TrajectoryGenerator.generateTrajectory(start, Waypoints, end, config);
+  }
+
+  /**
+   * @return Trajectory build by the constructor.
+   */
+  public final Trajectory getTrajectory() {
+    return trajectory;
+  }
+
+}
--- src/main/java/frc/robot/commands/drive/trajectories/TStraight.java
+++ src/main/java/frc/robot/commands/drive/trajectories/TStraight.java
@@ -7,47 +7,20 @@
 
 package frc.robot.commands.drive.trajectories;
 
-import java.util.ArrayList;
-
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
-import edu.wpi.first.wpilibj.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.util.Units;
 
 /**
- * Add your docs here.
+ * A straight path.
+ * Start at the origin and move 5 feet in the x direction.
  */
-public class TStraight{
+public class TStraight extends TBase {
 
-  /** Max speed of trajectory */
-  private static final double MAX_SPEED = Units.feetToMeters(5);
-  /** Max acceleration of trajectory (in m/s) */
-  private static final double MAX_ACCELERATION = Units.feetToMeters(5);
-  /** Whether the robot drives backwards along the path */
-  private static final boolean REVERSED = false;
-
-  private Pose2d start;
-  private Pose2d end;
-  private ArrayList<Translation2d> Waypoints = new ArrayList<Translation2d>();
-  private TrajectoryConfig config;
-
-  private Trajectory trajectory;
-
-  public TStraight() {
+  @Override
+  void build() {
     start = new Pose2d(Units.feetToMeters(0),Units.feetToMeters(0),Rotation2d.fromDegrees(0));
     end = new Pose2d(Units.feetToMeters(5),Units.feetToMeters(0),Rotation2d.fromDegrees(0));
-
-    config = new TrajectoryConfig(MAX_SPEED, MAX_ACCELERATION);
-    config.setReversed(REVERSED);
-
-    trajectory = TrajectoryGenerator.generateTrajectory(start, Waypoints, end, config);
-  }
-
-  public Trajectory getTrajectory() {
-    return trajectory;
   }
 
 }
