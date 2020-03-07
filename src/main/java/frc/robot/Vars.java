71cbc381f4be85070853dc12adcf43af825f850e
Joshua B <dogpetkid@gmail.com>
2020-03-07 14:28:00 -0700

    Shooter arm angle
    Move arm angle down

--- src/main/java/frc/robot/Vars.java
+++ src/main/java/frc/robot/Vars.java
@@ -66,7 +66,7 @@ public class Vars {
   // arm
   public static final double ARM_IN = 0; // degrees
   public static final double ARM_PLAYER = 43.68; // degrees
-  public static final double ARM_SHOOTING = 61.5; // degrees
+  public static final double ARM_SHOOTING = 64.5; // degrees
   public static final double ARM_OUT = 245; // degrees
   public static final double ARM_MINIMUM_ANGLE = -5; // degrees
   public static final double ARM_MAXIMUM_ANGLE = 265; // degrees
